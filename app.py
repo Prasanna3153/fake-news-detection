83b5b8d8bd382c7d353dbe98a7923617796b5903
Prasanna3153 <143609216+Prasanna3153@users.noreply.github.com>
2026-02-12 16:06:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Fake News Detection Project")
+import random
+
+print("Fake News Detection System")
+
+news = input("Enter a news headline: ")
+
+# Simple logic (demo purpose)
+if "government" in news.lower() or "official" in news.lower():
+    print("This news is likely Real")
+else:
+    print("This news might be Fake")
